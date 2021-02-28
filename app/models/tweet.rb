42e68b8d886cda3e55d9d0daad5c033c5e9cb89e
Bill Tihen <btihen@gmail.com>
2021-02-28 21:35:39 +0100

    new tweets are broadcast to all clients

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
   validates :body, presence: true
+
+  # broadcast "tweets" send all changes to the `turbo_stream` view "tweets"
+  # `broadcast_prepend_to` places new information at the top instead of appending to the bottom
+  after_create_commit { broadcast_prepend_to "tweets" }
 end
