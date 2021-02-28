c40a66d7f949f3a280566bb3369a1168b1e0721e
Bill Tihen <btihen@gmail.com>
2021-02-28 21:57:10 +0100

    broadcast deleted tweets via hotwire

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,5 @@ class Tweet < ApplicationRecord
   # broadcast "tweets" send all changes to the `turbo_stream` view "tweets"
   # `broadcast_prepend_to` places new information at the top instead of appending to the bottom
   after_create_commit { broadcast_prepend_to "tweets" }
+  after_destroy_commit { broadcast_remove_to "tweets" }
 end
