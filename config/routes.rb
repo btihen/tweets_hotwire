bf61e50504cc0427404be79e12828b73151490b4
Bill Tihen <btihen@gmail.com>
2021-02-28 19:47:59 +0100

    Tweets - Rails start for Single Page App (using hotwire)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tweets do
+    resource :like, only: [:create]
+    resource :retweet, only: [:create]
+  end
+  root to: "tweets#index"
 end
