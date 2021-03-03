e16f3c525f93e41130d40cf80932a1e6eaac9ea3
Bill Tihen <btihen@gmail.com>
2021-03-03 22:25:07 +0100

    turbo frame now allows in-place edit and cancel edit without reloading

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,4 +5,5 @@ class Tweet < ApplicationRecord
   # `broadcast_prepend_to` places new information at the top instead of appending to the bottom
   after_create_commit { broadcast_prepend_to "tweets" }
   after_destroy_commit { broadcast_remove_to "tweets" }
+  after_update_commit { broadcast_replace_to "tweets" }
 end
